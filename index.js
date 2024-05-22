393922f2990e796651d905f7880c65b25ea9413a
Thagman0 <thagman945@gmail.com>
2024-05-22 20:09:58 +0200

    Ajout du site

---
+++ index.js
@@ -0,0 +1,63 @@
+// Sélectionner tous les boutons avec la classe "btn"
+const buttons = document.querySelectorAll('.btn');
+
+// Ajouter un événement de survol (mouseenter) et de sortie (mouseleave) à chaque bouton
+buttons.forEach(btn => {
+  btn.addEventListener('mouseenter', animateButtonEnter);
+  btn.addEventListener('mouseleave', animateButtonLeave);
+});
+
+// Fonction pour animer le bouton à l'entrée du survol
+function animateButtonEnter(event) {
+  const button = event.target;
+  button.style.transform = 'scale(1.1)';
+  button.style.backgroundColor = '#ff8c8c'; // Changer la couleur de fond
+}
+
+// Fonction pour animer le bouton à la sortie du survol
+function animateButtonLeave(event) {
+  const button = event.target;
+  button.style.transform = 'scale(1)';
+  button.style.backgroundColor = '#ff6b6b'; // Revenir à la couleur de fond d'origine
+}
+
+
+// Sélectionner les éléments à animer
+const logoContainer = document.querySelector('.logo-container');
+const navLinks = document.querySelectorAll('nav a');
+
+// Ajouter les événements de survol
+logoContainer.addEventListener('mouseenter', () => {
+  logoContainer.classList.add('hover');
+});
+
+logoContainer.addEventListener('mouseleave', () => {
+  logoContainer.classList.remove('hover');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    link.classList.add('hover');
+  });
+
+  link.addEventListener('mouseleave', () => {
+    link.classList.remove('hover');
+  });
+});
+
+
+// Partie Présentation Membre
+
+document.addEventListener('DOMContentLoaded', function() {
+  const levels = document.querySelectorAll('.level1, .level2, .level3, .level4');
+
+  levels.forEach(level => {
+    level.addEventListener('mouseover', function() {
+      this.classList.add('active');
+    });
+
+    level.addEventListener('mouseout', function() {
+      this.classList.remove('active');
+    });
+  });
+});
